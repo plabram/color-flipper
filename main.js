e8f6b50a3ae070125062769d47a2c57b9db0b2e6
Penelope <freyalabram@gmail.com>
2023-05-24 17:13:16 +0200

    Add eslint and additional Prettier style guides

--- main.js
+++ main.js
@@ -5,47 +5,42 @@ const COLOR_PALETTE = {
   '#D264B6': 'Sky magenta',
   '#A480CF': 'Lavender',
   '#779BE7': 'Vista blue',
-  '#49B6FF': 'Argentinian blue',
+  '#49B6FF': 'Argentinian blue'
 };
 
 const addOptionsToColorPicker = () => {
-
-  const colorPickerSelect = document.querySelector('#color-picker')
+  const colorPickerSelect = document.querySelector('#color-picker');
 
   Object.keys(COLOR_PALETTE).forEach((color) => {
     const option = document.createElement('option');
     option.value = color;
     option.innerText = COLOR_PALETTE[color];
-    colorPickerSelect.append(option)
-  })
-}
+    colorPickerSelect.append(option);
+  });
+};
 
 const addEventListenerToColorPicker = () => {
   const colorPickerSelect = document.querySelector('#color-picker');
-  const colorName = document.querySelector("#color-name");
+  const colorName = document.querySelector('#color-name');
   colorPickerSelect.addEventListener('change', (ev) => {
-    const newColor = ev.target.value
+    const newColor = ev.target.value;
     document.body.style.backgroundColor = newColor;
-    const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`
+    const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`;
     colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : '-';
-
-  })
-}
+  });
+};
 
 const addEventListenerToHexColorPicker = () => {
   const colorPickerSelect = document.querySelector('#hex-color-picker');
-  const colorName = document.querySelector("#color-name");
+  const colorName = document.querySelector('#color-name');
   colorPickerSelect.addEventListener('input', (ev) => {
-    const newColor = ev.target.value
+    const newColor = ev.target.value;
     document.body.style.backgroundColor = newColor;
     const colorNameText = newColor;
     colorName.innerText = colorNameText;
+  });
+};
 
-  })
-}
-
-
-
-addOptionsToColorPicker()
-addEventListenerToColorPicker()
-addEventListenerToHexColorPicker()
+addOptionsToColorPicker();
+addEventListenerToColorPicker();
+addEventListenerToHexColorPicker();
